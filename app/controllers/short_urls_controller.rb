35086a1411d2ecd5943659d385ca2d84b00a3b63
Everaldo Gomes <everaldo.gomes@gmail.com>
2016-05-21 00:24:00 -0300

    Fix indentation. Remove unused files

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,23 +4,23 @@ class ShortUrlsController < ApplicationController
   end
 
   def create
-		@short_url = ShortUrl.new(short_url_params)
-		if @short_url.save
-	    render :new, notice: 'Sucesso'
-		else
-			render :new, error: 'Erro'
-		end
+    @short_url = ShortUrl.new(short_url_params)
+    if @short_url.save
+      render :new, notice: 'Sucesso'
+    else
+      render :new, error: 'Erro'
+    end
   end
 
 
-	def show
-		@short_url = ShortUrl.find_by(code: params[:code])
-		redirect_to @short_url.url
-	end
+  def show
+    @short_url = ShortUrl.find_by(code: params[:code])
+    redirect_to @short_url.url
+  end
 
-	private
+  private
 
-	def short_url_params
-		params.require(:short_url).permit(:url)
-	end
+  def short_url_params
+    params.require(:short_url).permit(:url)
+  end
 end
